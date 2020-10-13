23e0ee482b9bd494a5a2cce7ed3171f000b46377
David Odesola <faithodesola@gmail.com>
2020-10-14 02:06:44 +0800

    Deploying it with Firebase Hosting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./containers/Home/Home";
 import About from "./containers/About/About";
 import Works from "./containers/Works/Works";
 import Skills from "./containers/Skills/Skills";
-import Contact from "./containers/Contact/Contact";
+// import Contact from "./containers/Contact/Contact";
 import Footer from "./containers/Footer/Footer";
 import "./App.css";
 const App = () => {
@@ -13,7 +13,6 @@ const App = () => {
       <About />
       <Works />
       <Skills />
-      <Contact />
       <Footer />
     </div>
   );
